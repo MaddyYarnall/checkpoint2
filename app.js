b3a2eb396f77a52c2af0a4978d650b089f8ef142
Madison Yarnall <maddycodes01@gmail.com>
2021-12-10 22:05:06 -0700

    im so close i think

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ let money = 0;
 
 let clickUpgrades = {
   tools: {
-    price: 5,
+    price: 10,
     quantity: 0,
     multiplier: 2
   },
   advertisements: {
-    price: 5,
+    price: 20,
     quantity: 0,
     multiplier: 5
   }
@@ -15,12 +15,12 @@ let clickUpgrades = {
 
 let autoUpgrades = {
   mechanic: {
-    price: 350,
+    price: 100,
     quantity: 0,
     multiplier: 20
   },
   manager: {
-    price: 1000,
+    price: 250,
     quantity: 0,
     multiplier: 40
   }
@@ -32,29 +32,72 @@ function makeMoney(){
   console.log(money)
 }
 
+function toolsMoney(){
+  money += 2
+}
+
 function buyTools(){
   if(money >= clickUpgrades.tools.price){
     money -= clickUpgrades.tools.price
-    clickUpgrades.tools.quantity++
+    clickUpgrades.tools.quantity++ 
+    clickUpgrades.tools.price *= 3
+    toolsMoney()
   }
-  
 
 document.getElementById("totalMoney").innerHTML = money.toString()
 document.getElementById("toolInven").innerHTML = clickUpgrades.tools.quantity.toString()
+document.getElementById("toolsAmnt").innerHTML = clickUpgrades.tools.price.toString()
 }
 
 function buyAds(){
   if(money >= clickUpgrades.advertisements.price){
     money -= clickUpgrades.advertisements.price
     clickUpgrades.advertisements.quantity++
+    clickUpgrades.advertisements.price *= 3
   }
   
   
   document.getElementById("totalMoney").innerHTML = money.toString()
   document.getElementById("adInven").innerHTML = clickUpgrades.advertisements.quantity.toString()
+  document.getElementById("adsAmnt").innerHTML = clickUpgrades.advertisements.price.toString()
+}
+
+function buyMechanic(){
+  if(money >= autoUpgrades.mechanic.price){
+    money -= autoUpgrades.mechanic.price
+    autoUpgrades.mechanic.quantity++
+    autoUpgrades.mechanic.price *= 3
+    mechanicInterval()
+  }
+
+  document.getElementById("totalMoney").innerHTML = money.toString()
+  document.getElementById("mechInven").innerHTML = autoUpgrades.mechanic.quantity.toString()
+  document.getElementById("mechAmnt").innerHTML = autoUpgrades.mechanic.price.toString()
+}
+
+function mechanicInterval(){
+  setInterval(makeMoney, 5000)
 }
 
 
+
+function buyManager(){
+  if(money >= autoUpgrades.manager.price){
+    money -= autoUpgrades.manager.price
+    autoUpgrades.manager.quantity++
+    autoUpgrades.manager.price *= 3
+    managerInterval()
+  }
+  
+  
+  document.getElementById("totalMoney").innerHTML = money.toString()
+  document.getElementById("manInven").innerHTML = autoUpgrades.manager.quantity.toString()
+  document.getElementById("manAmnt").innerHTML = autoUpgrades.manager.price.toString()
+}
+
+function managerInterval(){
+  setInterval(makeMoney, 5000)
+}
 
 
 
